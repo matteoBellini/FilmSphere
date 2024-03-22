e8b43e29e44743e3551e1169641dbda12bd39b81
Matteo Bellini <matteo.bellini.2003@gmail.com>
2024-03-22 19:48:47 +0100

    Aggiunta operazione Rating
    Aggiunta  operazione Rating (da terminare)

--- SQL/Operazioni2.sql
+++ SQL/Operazioni2.sql
@@ -31,7 +31,9 @@ Lingua(NomeLingua)
 Audio(IDFile*, Lingua*)
 Sottotitolo(IDFile*, Lingua*)*/
 
+-- --------------------------------------
 -- Inserimento di un nuovo utente
+-- --------------------------------------
 DROP PROCEDURE IF EXISTS registrazione_utente;
 DELIMITER $$
 CREATE PROCEDURE registrazione_utente(IN _CF VARCHAR(16),IN _Nome VARCHAR(20),IN _Cognome VARCHAR(20),IN _DataNascita DATE,IN _Sesso VARCHAR(1),IN _Mail VARCHAR(50),
@@ -69,7 +71,9 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- --------------------------------------
 -- Emissione Fattura
+-- --------------------------------------
 DROP PROCEDURE IF EXISTS emissione_fattura;
 DELIMITER $$
 CREATE PROCEDURE emissione_fattura(IN _utente VARCHAR(16), IN _abb VARCHAR(10), IN _NumCarta)
@@ -85,7 +89,9 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- --------------------------------------
 -- Sottoscrizione del relativo abbonamento
+-- --------------------------------------
 DROP PROCEDURE IF EXISTS scelta_abbonamento;
 DELIMITER $$
 CREATE PROCEDURE scelta_abbonamento(IN _abb VARCHAR(10), IN _CF VARCHAR(16), IN _NumCarta VARCHAR(16), OUT _check BOOL)
@@ -126,7 +132,9 @@ BEGIN
 END $$
 DELIMITER ;
 
-/*Individuazione del server migliore ad ogni nuova connessione (Latitudine e Longitudine memorizzate in radianti)*/
+-- --------------------------------------
+-- Individuazione del server migliore ad ogni nuova connessione (Latitudine e Longitudine memorizzate in radianti)
+-- --------------------------------------
 DROP PROCEDURE IF EXISTS find_best_server;
 DELIMITER $$
 CREATE PROCEDURE find_best_server(IN Dispositivo VARCHAR(17), IN Latitudine FLOAT, IN Longitudine FLOAT, IN targetFile INTEGER, OUT resultServer VARCHAR(15), OUT _check BOOL)
@@ -301,7 +309,9 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- --------------------------------------
 -- Individuazione dei film che un utente può vedere in un certo paese (non sottoposti a restrizioni)
+-- --------------------------------------
 DROP PROCEDURE IF EXISTS VerificaRestrizioni;
 DELIMITER $$
 CREATE PROCEDURE VerificaRestrizioni(IN _IndirizzoMAC VARCHAR(17), IN _Utente VARCHAR(16), IN _Paese VARCHAR(20), IN _Film INTEGER, OUT _IDFile INTEGER)
@@ -351,7 +361,9 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- --------------------------------------
 -- Inserimento di nuovi dispositivi associati ad un utente, in linea con le specifiche del proprio abbonamento
+-- --------------------------------------
 DROP TRIGGER IF EXISTS InserimentoDispositivo;
 DELIMITER $$
 CREATE TRIGGER InserimentoDispositivo BEFORE INSERT ON Dispositivo
@@ -379,7 +391,9 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- --------------------------------------
 -- Invio di una notifica il giorno precedente della scadenza dell'abbonamento
+-- --------------------------------------
 CREATE TABLE UtentiInScadenza(
     Utente VARCHAR(16) NOT NULL PRIMARY KEY
 )ENGINE = InnoDB;
@@ -402,3 +416,109 @@ DROP EVENT IF EXISTS invioNotificaScadenza;
 CREATE EVENT invioNotificaScadenza ON SCHEDULE EVERY 1 DAY
 DO 
     CALL build_utenti_scadenza();
+
+-- --------------------------------------
+-- Rating di un film
+-- --------------------------------------
+-- Trigger relativo alle critiche
+DROP TRIGGER IF EXISTS rating_critiche;
+DELIMITER $$
+CREATE TRIGGER rating_critiche AFTER INSERT ON Critica
+FOR EACH ROW
+BEGIN
+    DECLARE punteggioCritica INTEGER DEFAULT 0;
+    DECLARE numCritiche INTEGER DEFAULT 0;
+
+    SELECT SUM(Punteggio) as s, COUNT(*) as n INTO punteggioCritica, numCritiche
+    FROM Critica
+    WHERE IDFilm = NEW.IDFilm;
+
+    UPDATE Film
+    SET Critica = punteggioCritica / numCritiche
+    WHERE ID = NEW.IDFilm;
+END $$
+DELIMITER ;
+
+-- Trigger relativo alle recensioni
+DROP TRIGGER IF EXISTS rating_recensioni;
+DELIMITER $$
+CREATE TRIGGER rating_recensioni AFTER INSERT ON Recensione
+FOR EACH ROW
+BEGIN
+    DECLARE punteggioRecensioni INTEGER DEFAULT 0;
+    DECLARE numeroRecensioni INTEGER DEFAULT 0;
+
+    SELECT Recensioni, NumRecensioni INTO punteggioRecensioni, numeroRecensioni
+    FROM Film
+    WHERE ID = NEW.IDFilm;
+
+    UPDATE Film
+    SET Recensioni = ((punteggioRecensioni * numeroRecensioni) + NEW.Punteggio) / (numeroRecensioni + 1),
+        NumRecensioni = numeroRecensioni + 1
+    WHERE ID = NEW.IDFilm;
+END $$
+DELIMITER ;
+
+-- Trigger relativo ai premi di un film
+DROP TRIGGER IF EXISTS rating_premi_film;
+DELIMITER $$
+CREATE TRIGGER rating_premi_film AFTER INSERT ON Vincita
+FOR EACH ROW
+BEGIN
+    DECLARE punteggioPremi INTEGER DEFAULT 0;
+    DECLARE numeroPremi INTEGER DEFAULT 0;
+
+    SELECT SUM(Importanza) as s, COUNT(*) as numPremi INTO punteggioPremi, numeroPremi
+    FROM Vincita V
+        INNER JOIN PremiFilm P
+        ON V.IDPremio = P.ID
+    WHERE V.IDFilm = NEW.IDFilm;
+
+    UPDATE Film
+    SET PremiFilm = punteggioPremi / numeroPremi
+    WHERE ID = NEW.IDFilm;
+END $$
+DELIMITER ;
+
+-- Trigger relativo ai premi di un cineasta
+-- DA FINIRE 
+DROP TRIGGER IF EXISTS rating_premi_cineasta;
+DELIMITER $$
+CREATE TRIGGER rating_premi_cineasta AFTER INSERT ON Premiazione
+FOR EACH ROW
+BEGIN
+    DECLARE finito1 INTEGER DEFAULT 0;
+    DECLARE finito2 INTEGER DEFAULT 0;
+
+    DECLARE cur1 CURSOR FOR
+        SELECT Film
+        FROM Recitazione
+        WHERE IdCineasta = NEW.IdCineasta;
+
+    DECLARE cur2 CURSOR FOR
+        SELECT Film
+        FROM Regia
+        WHERE IdCineasta = NEW.IdCineasta;
+
+    DECLARE HANDLER FOR NOT FOUND SET finito1 = 1;
+    DECLARE HANDLER FOR NOT FOUND SET finito2 = 1;
+END $$
+DELIMITER ;
+
+-- Procedure che restituisce il Rating di un film in ingresso
+DROP PROCEDURE IF EXISTS ratingFilm;
+DELIMITER $$
+CREATE PROCEDURE ratingFilm(IN _film INTEGER, OUT _rating DOUBLE)
+BEGIN
+    DECLARE ratingRecensioni INTEGER DEFAULT 0;
+    DECLARE ratingCritica INTEGER DEFAULT 0;
+    DECLARE ratingPremiFilm INTEGER DEFAULT 0;
+    DECLARE ratingPremiCineasta INTEGER DEFAULT 0;
+
+    SELECT Recensioni, Critica, PremiFilm, PremiCineasta INTO ratingRecensioni, ratingCritica, ratingPremiFilm, ratingPremiCineasta
+    FROM Film
+    WHERE ID = _film;
+
+    SET _rating = (0.2 * ratingRecensioni) + (0.4 * ratingCritica) + (0.4 * ((0.6 * ratingPremiFilm)+(0.4 * ratingPremiCineasta));
+END $$
+DELIMITER ;
